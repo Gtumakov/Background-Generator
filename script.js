2b182592f78db84b6138754cb88dce0ee1afa256
Gleb <gtumakov@gmail.com>
2018-03-19 23:16:03 +0200

    Add initial display of gradient.

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// Section 11 Lesson 100
+/* Below you will find the 3 files for the Background Generator. A few things you can try:
+
+1. Write code so that the colour inputs match the background generated on the first page load.
+
+2. Display the initial CSS linear gradient property on page load.
+
+3. BONUS: Add a random button which generates two random numbers for the colour inputs.
+
+Good luck */
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
@@ -6,19 +17,29 @@ var button = document.querySelector('.resetBtn');
 var random = document.querySelector(".randomColor");
 
 
+
+
+
 function setGradient() {
   body.style.backgroundImage = 'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
   css.style.display = "block";
   css.textContent = body.style.backgroundImage + ';';
 }
 
+function resetColors() {
+  body.style.backgroundImage = 'linear-gradient(to right, rgb(0, 255, 0), rgb(255, 0, 0))';
+  css.style.display = "block";
+  css.textContent = body.style.backgroundImage + ';';
+}
+
 // Reset styles when we press the button
-button.addEventListener("click", function() {
-  body.style.backgroundImage = 'linear-gradient(to right, red, yellow)';
-  css.style.display = "none";
-  color1.value = "#00ff00";
-  color2.value = "#ff0000";
-});
+// button.addEventListener("click", function() {
+    // body.style.backgroundImage = 'linear-gradient(to right, rgb(0, 255, 0), rgb(255, 0, 0))';
+  // body.style.backgroundImage = 'linear-gradient(to right, red, yellow)';
+  // css.style.display = "none";
+  // color1.value = "#00ff00";
+  // color2.value = "#ff0000";
+// });
 
 
 // TWO FUNCTIONS TO CALL FOR GENERATING RANDOM RGB COLORS
@@ -36,8 +57,9 @@ function getRandom() {
   css.textContent = body.style.backgroundImage + ';';
 }
 
+button.addEventListener("click", resetColors);
 
-
+body.addEventListener("cached", setGradient());
 
 random.addEventListener("click", getRandom);
 
